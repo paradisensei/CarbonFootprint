6e50efd0274dc74221d4dc4bf20234d87d4f142f
Alexander Pliskin <alexnetmore@gmail.com>
2017-10-27 23:56:17 +0300

    watcher added

--- src/geolocation.js
+++ src/geolocation.js
@@ -6,14 +6,44 @@ import {
 } from 'react-native';
 
 export default class MyGeolocation extends Component {
+
+  constructor() {
+    super(props);
+
+    this.state = {
+      latitude: null,
+      longitude: null,
+      error: null,
+    };
+  }
+
   componentDidMount() {
-    navigator.geolocation.getCurrentPosition(
+    this.watchId = navigator.geolocation.watchPosition(
       (position) => {
-        console.log(position);
-        var initialPosition = JSON.stringify(position);
-        console.log(initialPosition);
-      }
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        });
+        console.log(JSON.stringify(position));
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 2000, maximumAge: 1000, distanceFilter: 10 },
     );
+  }
+
+  // componentDidMount() {
+  //   navigator.geolocation.getCurrentPosition(
+  //     (position) => {
+  //       console.log(position);
+  //       var initialPosition = JSON.stringify(position);
+  //       console.log(initialPosition);
+  //     }
+  //   );
+  // }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchId);
   }
 
   render() {
